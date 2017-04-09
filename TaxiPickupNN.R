9a5f97fe1881006e144373270d80aa666daeb60c
ciaranporter5 <ciaran.porter5@mail.dcu.ie>
2017-04-09 23:28:04 +0100

    Update TaxiPickupNN.R

--- TaxiPickupNN.R
+++ TaxiPickupNN.R
@@ -1,51 +1,76 @@
+# Run Neural Network on Training Data and Test Data
+# Check error on training data using Root Mean Squared Error (RMSE) and
+# Mean Absolute Error (MAE). If this is too low on training, risk of overfitting.
+# There should be some element of error in the training set,
+# but the test set error should not be much higher for model to be reasonable accurate.
+
+#install.packages("nnet")
 library(data.table)
 library(plyr)
 library(caret)
 library(caTools)
 library(nnet)
 
-# Running of neural net model on Taxi Pickup data
-set.seed(1)
-
-# Split into training and test data based off of split Boolean Vector
-TaxiPickupSummary$TaxiSplit = sample.split(
-  TaxiPickupSummary$Num_Jrnys, SplitRatio = 0.70)
-TaxiTrain = subset(TaxiPickupSummary, TaxiSplit == TRUE)
-TaxiTest = subset(TaxiPickupSummary, TaxiSplit == FALSE)
-
-# scale the continuous variables in the training/test dataset (mean=0, sd=1)
-TaxiTrain$Num_Jrnys_Scaled <- scale(TaxiTrain$Num_Jrnys)
-TaxiTrain$total_passenger_count_Scaled<- scale(TaxiTrain$total_passenger_count)
-TaxiTrain$total_fare_Scaled <- scale(TaxiTrain$total_fare)
-
-TaxiTest$Num_Jrnys_Scaled <- scale(TaxiTest$Num_Jrnys)
-TaxiTest$total_passenger_count_Scaled<- scale(TaxiTest$total_passenger_count)
-TaxiTest$total_fare_Scaled <- scale(TaxiTest$total_fare)
-
-TaxiTestScaleCompare <- as.data.frame(TaxiTest[c(7,69)])
-#write.csv(TaxiTestScaleCompare, file = "TaxiTestScale.csv", row.names = FALSE)
-
 # Run the neural net on the scaled training dataset
-TaxiNnet <- nnet(TaxiTrain[-c(1,2,3,5,6,7,8,9,68,69)], TaxiTrain[69], size =1, 
-                 linout = TRUE, maxit=130)
+# check if convergence occurs or need to introduce max iterations
+# Run set seed each time with nnet as will use random inputs each time otherwise
+set.seed(2)
+TaxiNnet <- nnet(TaxiTrain[-c(1,2,3,4,5,6,7,8,9,68,69)], TaxiTrain[69], size =1, 
+                 linout = TRUE, decay = 0.001)
 
+
+# 1. Checks Made on Training Data
 # predict on test data and compare results
-TaxiPredict <- predict(TaxiNnet,TaxiTest[-c(1,2,3,5,6,7,8,9,68,69)])
-head(TaxiPredict)
-head(data.frame(TaxiTest$Num_Jrnys_Scaled,TaxiPredict))
+TaxiPredict <- predict(TaxiNnet,TaxiTrain[-c(1,2,3,4,5,6,7,8,9,68,69)])
+TaxiPredict_DF<- data.frame(TaxiPredict)
+
+# descale the predictions and show inital outputs
+TaxiPredict_DF$Descaled <- ifelse(minMaxDescaling(TaxiPredict_DF$Num_Jrnys_Scaled, 
+                                        max(TaxiTrain$Num_Jrnys), 
+                                        min(TaxiTrain$Num_Jrnys))>0,
+                                  minMaxDescaling(TaxiPredict_DF$Num_Jrnys_Scaled, 
+                                                  max(TaxiTrain$Num_Jrnys), 
+                                                  min(TaxiTrain$Num_Jrnys)),0)
+
+head(data.frame(TaxiTrain$Num_Jrnys, TaxiPredict_DF$Descaled),10)
 
 # calculate RMSE on scaled data
-taxi.nn_rmse <- sqrt(mean((TaxiPredict-TaxiTest$Num_Jrnys_Scaled)^2))
+taxi.nn_rmse <- sqrt(mean((TaxiPredict_DF$Descaled-TaxiTrain$Num_Jrnys)^2))
 taxi.nn_rmse
 
 # calculate Mean Absolute Error on scaled data
-taxi.nn_mae <- mean(abs(TaxiPredict-TaxiTest$Num_Jrnys_Scaled))
+taxi.nn_mae <- mean(abs(TaxiPredict_DF$Descaled-TaxiTrain$Num_Jrnys))
 taxi.nn_mae
 
-taxi.nn_predicted <- data.frame(TaxiTest$Num_Jrnys_Scaled,TaxiPredict,
-                                TaxiTest$Num_Jrnys_Scaled - TaxiPredict,
-                                abs((TaxiTest$Num_Jrnys_Scaled - TaxiPredict)/
-                                  TaxiTest$Num_Jrnys_Scaled))
-colnames(taxi.nn_predicted) <- c("Original", "Predicted", "Difference", "Perc Diff")
-write.csv(taxi.nn_predicted, file = "TaxiNNPredicted.csv", row.names = FALSE)
+# 2. Checks Made on Test Data
+# predict on test data and compare results
+TaxiPredict <- predict(TaxiNnet,TaxiTest[-c(1,2,3,4,5,6,7,8,9,68,69)])
+TaxiPredict_DF<- data.frame(TaxiPredict)
 
+# descale the predictions and show inital outputs
+TaxiPredict_DF$Descaled <- ifelse(minMaxDescaling(TaxiPredict_DF$Num_Jrnys_Scaled, 
+                                                  max(TaxiTest$Num_Jrnys), 
+                                                  min(TaxiTest$Num_Jrnys))>0,
+                                  minMaxDescaling(TaxiPredict_DF$Num_Jrnys_Scaled, 
+                                                  max(TaxiTest$Num_Jrnys), 
+                                                  min(TaxiTest$Num_Jrnys)),0)
+
+head(data.frame(TaxiTest$Num_Jrnys, TaxiPredict_DF$Descaled),10)
+
+# calculate RMSE on scaled data
+taxi.nn_rmse <- sqrt(mean((TaxiPredict_DF$Descaled-TaxiTest$Num_Jrnys)^2))
+taxi.nn_rmse
+
+# calculate Mean Absolute Error on scaled data
+taxi.nn_mae <- mean(abs(TaxiPredict_DF$Descaled-TaxiTest$Num_Jrnys))
+taxi.nn_mae
+
+taxi.nn_predicted <- data.frame(TaxiTest$pickup_Geohash, TaxiTest$pickupDate,
+                                TaxiTest$TimeInterval,TaxiTest$Num_Jrnys,
+                                TaxiPredict_DF$Descaled,
+                                TaxiTest$Num_Jrnys - TaxiPredict_DF$Descaled,
+                                abs((TaxiTest$Num_Jrnys - TaxiPredict_DF$Descaled)/
+                                  TaxiTest$Num_Jrnys))
+colnames(taxi.nn_predicted) <- c("Geohash","Date", "Time", "Original", 
+                                 "Predicted", "Difference", "Perc Diff")
+write.csv(taxi.nn_predicted, file = "TaxiNNPredicted.csv", row.names = FALSE)
