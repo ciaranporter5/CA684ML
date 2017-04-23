81b97c549e1451df980e4bfb928f829f597d0058
vanrhel2 <luciano.vanrheenen2@mail.dcu.ie>
2017-04-23 23:04:14 +0100

    Add files via upload

---
+++ Weather Imputation.R
@@ -0,0 +1,28 @@
+#install.packages("readxl")
+#install.packages("UsingR")
+#install.packages("missForest")
+library(readxl)
+library(UsingR)
+library(missForest)
+
+#Set the working directory
+setwd("C:\\Users\\lvanrheena007\\Documents\\DCU\\CA684 ML")
+
+#read in weather data
+weather <- read_excel("Weather_Interim.xlsx", sheet = "Interim")
+
+#Check weather data for missing values and overall distribution
+summary(weather)
+densityplot(weather)
+sum(is.na(weather))
+
+#find missing values - Temporal imputation for records with no values
+pMiss <­ function(x){sum(is.na(x))/length(x)*100}
+apply(weather,1,pMiss)
+
+#find missing values - Random Forest imputation for missing records with some values
+weather.mis <- prodNA(weather, noNA = 0.1)
+weather.imp <- missForest(weather.mis)
+
+#output imputed weather data
+write.csv(weather.imp.$ximp, "Weather_Complete.csv")
